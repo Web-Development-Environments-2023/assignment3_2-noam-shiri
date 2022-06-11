2640d46724ad99fee720e39c3086362d5065d841
Shiri Itach <shiritach@gmail.com>
2022-06-11 17:40:49 +0300

    get and save family recipes

--- routes/utils/user_utils.js
+++ routes/utils/user_utils.js
@@ -26,6 +26,17 @@ async function get3LastWatchedRecipes(user_id){
     return recipes_id;
 }
 
+async function getFamilyRecipes(user_id){
+    return await DButils.execQuery(`SELECT * FROM recipe WHERE user_id=${user_id};`);
+    }
+
+async function saveFamilyRecipe(user_id, recipe_info){
+    await DButils.execQuery(`INSERT INTO recipe 
+        (recipename, preperationTimeMinutes, picture, popularity, isVegan, isVegetarian, hasGluten, user_id, isFamilyRecipe)
+        VALUES ('${recipe_info.title}','${recipe_info.readyInMinutes}','${recipe_info.image}','${recipe_info.popularity}',
+        ${recipe_info.vegan},${recipe_info.vegetarian},${recipe_info.glutenFree},'${user_id}', true);`);
+    }
+
 
 exports.markAsFavorite = markAsFavorite;
 exports.getFavoriteRecipes = getFavoriteRecipes;
@@ -33,3 +44,5 @@ exports.checkIfFavoriteRecipes = checkIfFavoriteRecipes;
 exports.get3LastWatchedRecipes = get3LastWatchedRecipes;
 
 exports.markAsWatched = markAsWatched;
+exports.saveFamilyRecipe = saveFamilyRecipe;
+exports.getFamilyRecipes = getFamilyRecipes;
